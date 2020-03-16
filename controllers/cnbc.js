3cf65f0389bf73b589086d5f5c5261fe1db3832a
Eric Benedetto <ericsplace3@gmail.com>
2020-03-16 09:03:03 -0400

    make it better

---
+++ controllers/cnbc.js
@@ -0,0 +1,25 @@
+const BreakingNews = require('../models/BreakingNews');
+
+exports.articleExistsCNBC = async (a, b) => {
+    try {
+        const article = await BreakingNews.findOne({ title: a, url: b });
+        if (!article) {
+            return false;
+        }
+        return true;
+    } catch (err) {
+        console.log(err);
+        return true;
+    }
+}
+
+exports.addArticle = async a => {
+    try {
+        await BreakingNews.create(a);
+
+        return true;
+    } catch (err) {
+        console.log(err);
+        return false;
+    }
+}
---
+++ index.js
@@ -0,0 +1,39 @@
+require('dotenv').config({ path: './config/.local.env' });
+const fetch = require('node-fetch');
+const Discord = require('discord.js');
+const connectDB = require('./config/db');
+const { articleExistsCNBC, addArticle } = require('./controllers/cnbc');
+const bot = new Discord.Client();
+const { TOKEN, NEWS_API_URL, UPDATE_TIME, CHANNEL_NAME } = process.env;
+
+bot.login(TOKEN);
+
+connectDB();
+
+let channel;
+bot.on('ready', () => {
+    const channel_map = bot.channels.cache.filter(({ type, name }) => type === 'text' && name === CHANNEL_NAME);
+    const channel_id = channel_map.toJSON()[0].id
+    channel = bot.channels.cache.get(channel_id);
+    channel.send('test')
+
+    setInterval(() => { getNews() }, UPDATE_TIME * 1000);
+});
+
+const getNews = async () => {
+    try {
+        const res = await fetch(NEWS_API_URL)
+        const json = await res.json();
+        const { data: { newsAlert: { breakingNews } } } = json;
+        if (breakingNews && !await articleExistsCNBC(breakingNews.title, breakingNews.url)) {
+            addArticle({
+                title: breakingNews.title,
+                url: breakingNews.url
+            })
+            channel.send(breakingNews.url)
+        }
+    } catch (err) {
+        console.log(err)
+        return err;
+    }
+}
